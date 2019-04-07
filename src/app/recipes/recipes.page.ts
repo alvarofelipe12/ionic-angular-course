7aef5038db47ebc8059dc6ee3faf5f5a5653ce68
Alvaro Felipe Garcia Mendez <agarcia@uva3.com>
2019-04-07 14:35:30 -0500

    First page done

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
     selector: 'app-recipes',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+    recipes: Recipe[] = [
+        {
+            id: 'r1',
+            title: 'Schnitzel',
+            imageURL:
+                'https://upload.wikimedia.org/wikipedia/commons/4/40/Wiener_Schnitzel.jpg',
+            ingredients: ['French Fries', 'Pork Meat', 'Salad'],
+        },
+        {
+            id: 'r2',
+            title: 'Spaghetti',
+            imageURL:
+                'https://cdn.pixabay.com/photo/2011/04/29/11/20/spaghetti-7113_1280.jpg',
+            ingredients: ['Spaghetties', 'Meat', 'Tomatoes'],
+        },
+    ];
+
     constructor() {}
 
     ngOnInit() {}
