f3c2a81a74f76c6601d24aaddd1ea6e64bf776a1
Alvaro Felipe Garcia Mendez <agarcia@uva3.com>
2019-04-07 14:19:49 -0500

    Indent config
    Indent config and test

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.page.html',
-  styleUrls: ['./recipes.page.scss'],
+    selector: 'app-recipes',
+    templateUrl: './recipes.page.html',
+    styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
